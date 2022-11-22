434a241de469ad8d667b44d817da8920809c1742
graziacrp <graziacarpineta25@gmail.com>
2022-11-22 16:18:20 +0100

    Aggiungere i file di progetto.

---
+++ BookStoreCSharp/Program.cs
@@ -0,0 +1,25 @@
+string titolo = "Colazione da Tiffany";
+string autore = " Truman Capote";
+string isbn = "978-8811683452";
+int pagine = 128 ;
+int peso = 220;
+float lunghezza = 22.3f;
+float spessore = 1.6f;
+float altezza = 14.9f;
+float valutazione = 4.5f;
+int numeroRecensioni = 39;
+string kindle = "No";
+string copertinaFlessibile = "Sì";
+
+Console.WriteLine("Titolo" + titolo + "\n");
+Console.WriteLine("Autore" + autore + "\n");
+Console.WriteLine("Isbn" + isbn + "\n");
+Console.WriteLine("Pagine" + pagine + "\n");
+Console.WriteLine("Peso" + peso + "\n");
+Console.WriteLine("Lunghezza" + lunghezza + "\n");
+Console.WriteLine("Spessore" + spessore + "\n");
+Console.WriteLine("Altezza" + altezza + "\n");
+Console.WriteLine("Valutazione" + valutazione + "\n");
+Console.WriteLine("Numero recensioni" + numeroRecensioni + "\n");
+Console.WriteLine("Kindle" + kindle + "\n");
+Console.WriteLine("Copertina flessibile" + copertinaFlessibile + "\n");
